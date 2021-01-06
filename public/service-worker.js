eaf5de1e7f36e54b0585381f8abeb16e8e8d21e6
Cesar Martinez <chernanma@msn.com>
2021-01-05 22:33:29 -0500

    deploying

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,7 +8,8 @@ const FILES_TO_CACHE = [
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
   "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
-  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0" 
+  "https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0", 
 ];
 
 const CACHE_NAME = "static-cache-v1";
